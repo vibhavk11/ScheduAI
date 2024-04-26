8c96c29575d6967486fa67881dbe2b14404b8f53
Addison Douglas <addisondouglas850@gmail.com>
2024-04-26 16:44:57 -0700

    [ENG-1] Logout Button

--- app/scheduaiapp/src/App.tsx
+++ app/scheduaiapp/src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
 import './login.tsx'
 import LoginButton from './login.tsx'
+import LogoutButton from './assets/logout.tsx'
+import { Stack } from '@mui/material'
 
 function App() {
-  return <LoginButton />
+  return (
+    <Stack direction={'column'}>
+      <LoginButton /> <LogoutButton />
+    </Stack>
+  )
 }
 
 export default App
