cebb544f80dffcf1e9ead8435a1a85904974b42d
Addison Douglas <addisondouglas850@gmail.com>
2024-04-26 16:40:10 -0700

    [ENG-1] Login Page

--- app/scheduaiapp/src/main.tsx
+++ app/scheduaiapp/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
+  <Auth0Provider
+    domain="dev-rdrecx1yiezpxvgs.us.auth0.com"
+    clientId="djeB9ic2ntMuCepze322IilTdMKYOORm"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
 )
