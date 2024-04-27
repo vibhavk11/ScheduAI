191c117cc91199c5dd4629534b4e5db80181439a
Vibhav Kalyan <vkalyanhorizen@gmail.com>
2024-04-26 17:01:55 -0700

    [ENG-2] Backend Creation

--- app/scheduaiapp/src/main.tsx
+++ app/scheduaiapp/src/main.tsx
@@ -1,17 +1,18 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App'
+import ApolloClientComponent from './components/ApolloClient/ApolloClient'
 
-const root = createRoot(document.getElementById('root')!)
-
-root.render(
-  <Auth0Provider
-    domain="dev-rdrecx1yiezpxvgs.us.auth0.com"
-    clientId="djeB9ic2ntMuCepze322IilTdMKYOORm"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <App />
-  </Auth0Provider>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ApolloClientComponent>
+    <Auth0Provider
+      domain="dev-rdrecx1yiezpxvgs.us.auth0.com"
+      clientId="djeB9ic2ntMuCepze322IilTdMKYOORm"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </ApolloClientComponent>,
 )
