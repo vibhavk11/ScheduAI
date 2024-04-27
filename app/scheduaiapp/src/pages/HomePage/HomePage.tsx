fc48f028157bd20c93c45416c3617866dd9e580f
Vibhav Kalyan <vkalyanhorizen@gmail.com>
2024-04-26 19:46:27 -0700

    [ENG-4] Backend and Frontend communication

--- app/scheduaiapp/src/pages/HomePage/HomePage.tsx
+++ app/scheduaiapp/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,18 @@
 import { Button, Container, Stack } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import LoginButton from '../../login'
+import { useQuery } from '@apollo/client'
+import { TestQueryDocument } from '../../graphql/generated/graphql'
 
 const HomePage = () => {
   const navigate = useNavigate()
 
+  const { data, loading, error } = useQuery(TestQueryDocument)
+
   return (
     <Stack direction={'column'}>
       <Container>
-        <h1>Welcome to ScheduAI</h1>
+        <h1>{data?.test}</h1>
         <p>Click the button below to log in</p>
         <LoginButton />
         <Button onClick={() => navigate('/loggedout')}>Logout</Button>
